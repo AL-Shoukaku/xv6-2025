385d22f8098afbdce7192e96a00db61d10295cbf
shoukaku <abcd791516316@163.com>
2026-07-14 16:38:28 +0800

    完成了lab1的exec，至此通过了所有的官方测试

--- user/find.c
+++ user/find.c
@@ -3,8 +3,10 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
+#include "kernel/param.h"
 
 void find(char *path, char *name);
+void findexec(char *path, char *name, char **argv, int *argNum);
 char* fmtname(char *path);
 
 int main(int argc, char *argv[])
@@ -13,6 +15,42 @@ int main(int argc, char *argv[])
         write(2, "argument error!\n", 15);
         exit(1);
     }
+    int pos = 0;
+    for (int i = 3;i < argc;i++) {
+        if (strcmp("-exec", argv[i]) == 0) {
+            pos = i;
+            break;
+        }
+    }
+
+    //存在-exec参数
+    if (pos > 0) {
+        char* newArgv[MAXARG];
+        int argNum = 0;
+        //先构建指令参数
+        for (int i = pos + 1; i < argc; i++) {
+            newArgv[argNum++] = argv[i];
+        }
+        //构建find指令找到的文件名作为参数
+        for (int i = 2; i < pos; i++) {
+            findexec(argv[1], argv[i], newArgv, &argNum);
+        }
+
+        int pid = fork();
+        if (pid > 0) {
+            wait((int *) 0);
+        } else if (pid == 0){
+           exec(argv[pos + 1], newArgv);
+           printf("findexec: exec error!\n");
+           exit(1);
+        } else {
+            printf("fork error!\n");
+            exit(1);
+        }
+        exit(0);
+    }
+
+    //不存在-exec参数，正常执行
     char* path = argv[1];
     for (int i = 2;i < argc;i++) {
         char* filename = argv[i];
@@ -65,6 +103,61 @@ void find(char *path, char *name) {
     }
     close(fd);
     return;
+}
+
+void findexec(char *path, char *name, char **argv, int *argNum) {
+    int fd;
+    struct stat st;
+    char buf[512], *p;
+    struct dirent de;
+    if (strcmp(fmtname(path), name) == 0) {
+        if (*argNum + 1 == MAXARG) {
+            printf("findexec: too much argument!\n");
+            return;
+        }
+        //这里不能直接用path！因为*path的值后面会被改变！
+        char s[512];
+        strcpy(s, path);
+        argv[*argNum] = s;
+        *argNum += 1;
+    }
+
+    if((fd = open(path, O_RDONLY)) < 0){
+        fprintf(2, "findexec: cannot open %s\n", path);
+        return;
+    }
+
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "findexec: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+    // 对于非目录的直接关闭fd，否则会数量不够 
+    if (st.type != T_DIR) {
+        close(fd);
+        return;
+    }
+            
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+        printf("findexec: path too long\n");
+        return;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+        if (de.inum == 0) {
+            continue;
+        }
+        if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) {
+            continue;
+        }
+        memmove(p, de.name, DIRSIZ);
+        p[strlen(de.name)] = '\0';
+        findexec(buf, name, argv, argNum);
+    }
+    close(fd);
+ 
 }
 
 char* fmtname(char *path)
